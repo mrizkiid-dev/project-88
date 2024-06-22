74deeefb1a8ae7f598596a39e9c520562a439804
muhammad rizki <muhammad.rizki.surel.dev@gmail.com>
2024-06-22 22:49:36 +0700

    fix(search): fix loading search in mobile

--- pages/search-page/index.vue
+++ pages/search-page/index.vue
@@ -1,8 +1,11 @@
 <template>
     <AppbarSearchMobile :is-typing-active="true" :is-gray="true" in-style="bg-third-color border-b-[1px] border-primary" v-model="searchAppBar"/>
-    <Loading v-if="isSearchLoading" />
-    <div v-else class="px-2 flex flex-col gap-2 py-[100px]">
-      <SearchSuggestion v-if="searchResult && searchResult.length > 0" v-for="card in searchResult" :key="card.id" :id="card.id" :title="card.title" :image-src="card.image" />
+    <!-- <Loading v-if="isSearchLoading" /> -->
+    <div class="px-2 flex flex-col gap-2 py-[100px]">
+      <div v-if="isSearchLoading" class="flex justify-center">
+          <Icon name="eos-icons:loading" size="25" />
+      </div>
+      <SearchSuggestion v-else-if="searchResult && searchResult.length > 0" v-for="card in searchResult" :key="card.id" :id="card.id" :title="card.title" :image-src="card.image" />
     </div>
     <Bottomnav />
 </template>
