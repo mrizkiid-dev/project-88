d68b916494e0fec3c61c85dd967f6a9378d4e31a
muhammad rizki <muhammad.rizki.surel.dev@gmail.com>
2024-07-05 18:01:39 +0700

    fix(pm2): adding env settings into ecosystem.config.cjs pm2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,7 +5,10 @@ module.exports = {
         port: '3000',
         exec_mode: 'cluster',
         instances: 'max',
-        script: './.output/server/index.mjs'
+        script: './.output/server/index.mjs',
+        env_staging: {
+          NODE_ENV: "staging"
+        },
       }
     ]
   }
